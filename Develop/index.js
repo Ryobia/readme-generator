f53eb2682bba795e2768134e0c9e7e7a0335eb00
Ryobia <ryobia36@gmail.com>
2021-03-18 12:50:45 -0600

    Added questions and writefile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,112 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input', //Readme Title
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your project title!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input', //Readme Description
+        name: 'description',
+        message: 'Please provide a description of your project. (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please provide a description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm', //Readme installation instructions
+        name: 'confirmInstall',
+        message: 'Would you like to include Installation Instructions?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter Installation Instructions:',
+        when: ({ confirmInstall }) => confirmInstall
+      },
+      {
+        type: 'confirm', //Readme Usage information
+        name: 'confirmUsage',
+        message: 'Would you like to include Usage Information?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter Usage Information:',
+        when: ({ confirmUsage }) => confirmUsage
+      },
+      {
+        type: 'confirm', //Readme Contribution Guidelines
+        name: 'confirmContribute',
+        message: 'Would you like to include Contribution Guidelines?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please enter Installation Instructions:',
+        when: ({ confirmContribute }) => confirmContribute
+      },
+      {
+        type: 'confirm', //Readme Test Instructions
+        name: 'confirmTest',
+        message: 'Would you like to include Test Instructions?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter Test Instructions:',
+        when: ({ confirmTest }) => confirmTest
+      }
+      
+    ]);
+  };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(`./dist/${fileName}.md`, data, err => {
+        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+          reject(err);
+          // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+          return;
+        }
+  
+        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+        resolve({
+          ok: true,
+          message: 'README Generated!'
+        });
+      });
+    });
+  };
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+promptUser();
